f84c7c89705a9081af6743d496eb870e03337ba9
umaicode <park.seungchan99@gmail.com>
2025-11-14 15:45:56 +0900

    fix: 정기예금 정보 및 옵션 출력 view 함수 수정

--- finlife/views.py
+++ finlife/views.py
@@ -4,7 +4,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import DepositProducts, DepositOptions
-from .serialilzers import DepositProductsSerializer, DepositOptionsSerializer, TopRateSerializer
+from .serializers import DepositProductsSerializer, DepositOptionsSerializer, TopRateSerializer
 from django.shortcuts import get_object_or_404, get_list_or_404
 
 
