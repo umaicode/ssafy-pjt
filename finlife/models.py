2e3c7204e91d1f04e77d9d842f2eeb73c872e827
umaicode <park.seungchan99@gmail.com>
2025-11-14 15:41:07 +0900

    fix: DepositProducts, DepositOptions 모델 재정의

--- finlife/models.py
+++ finlife/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class DepositProducts(models.Model):
     # 금융 상품 코드
     # PRIMARY KEY
-    fin_prdt_cd = models.CharField(max_length=50, primary_key=True)
+    fin_prdt_cd = models.CharField(max_length=50)
     # 금융회사명
     kor_co_nm = models.CharField(max_length=100)
     # 상품명
@@ -13,23 +13,27 @@ class DepositProducts(models.Model):
     # 기타 참고사항
     etc_note = models.TextField()
     # 금리 (default = -1)
-    max_limit = models.IntegerFIeld(default=-1)
-    # # 가입 제한
-    # join_deny = models.IntegerField()
-    # # 가입 대상
-    # join_member = models.TextField()
-    # # 가입방법
-    # join_way = models.TextField()
-    # # 우대조건
-    # spcl_cnd = models.TextField()
+    max_limit = models.IntegerField(default=-1)
+    # 가입 제한
+    join_deny = models.IntegerField(default=0)
+    # 가입 대상
+    join_member = models.CharField(max_length=100)
+    # 가입방법
+    join_way = models.CharField(max_length=100) 
+    # 우대조건
+    spcl_cnd = models.TextField(max_length=500)
 
 
 class DepositOptions(models.Model):
     # 상품 코드
     product = models.ForeignKey(DepositProducts, on_delete=models.CASCADE)
-    # 저축 기간 (월)
-    save_trm = models.IntegeField()
+    # 이자율 유형명
+    intr_rate_type_nm = models.CharField(max_length=100)
     # 이자율
     intr_rate = models.FloatField()
     # 최고 우대 금리
     intr_rate2 = models.FloatField()
+    # 저축 기간 (월)
+    save_trm = models.IntegerField()
+    # 금융 상품 코드
+    fin_prdt_cd = models.CharField(max_length=50)
