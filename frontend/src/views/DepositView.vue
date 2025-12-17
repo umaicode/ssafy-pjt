278524fa264dc8020324b6d8401c98ebc00477a6
umaicode <park.seungchan99@gmail.com>
2025-12-18 03:02:13 +0900

    add: DepositView, DepositList, DepositListItem 구현

--- frontend/src/views/DepositView.vue
+++ frontend/src/views/DepositView.vue
@@ -1,8 +1,21 @@
 <template>
-    <h1>금융상품</h1>
+    <div>
+        <h1>금융상품</h1>
+        <DepositList />
+    </div>
 </template>
 
 <script setup>
+    import { onMounted } from 'vue';
+    import { useProductStore } from '@/stores/products';
+    import { RouterLink } from 'vue-router';
+    import DepositList from '@/components/DepositList.vue';
+
+    const store = useProductStore()
+
+    onMounted(() => {
+        store.getProducts()
+    })
 
 </script>
 
