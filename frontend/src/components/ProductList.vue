19542a499c12df707000a129a723ed879fec287c
umaicode <park.seungchan99@gmail.com>
2025-12-18 04:18:02 +0900

    fix: saving 포함하여 product로 전부 변경

--- frontend/src/components/ProductList.vue
+++ frontend/src/components/ProductList.vue
@@ -1,18 +1,22 @@
 <template>
   <div>
     <h3>금융상품 리스트</h3>
-    <DepositListItem 
-      v-for="deposit in store.products"
-      :key="deposit.id"
-      :deposit="deposit"
+    <ProductListItem 
+      v-for="product in items"
+      :key="product.id"
+      :product="product"
     />
   </div>
 </template>
 
 <script setup>
-  import DepositListItem from '@/components/DepositListItem.vue';
+  import ProductListItem from '@/components/ProductListItem.vue';
   import { useProductStore } from '@/stores/products';
 
+  defineProps({
+    items: Array
+  })
+  
   const store = useProductStore()
 </script>
 
