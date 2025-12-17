398e1b052ba0ee314030e3a65fdab75e8dd16881
hyeonji <sahacar2000@naver.com>
2025-12-18 00:56:34 +0900

    add: 금융상품 메뉴추가 마이페이지는 주석처리

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,10 +1,17 @@
 <template>
   <div>
-
+    <!-- 메뉴 -->
+    <nav>
+      <RouterLink :to="{name:'DepositView'}">금융상품</RouterLink>
+      <!-- <RouterLink :to="{name:'ProfileView'}">마이페이지</RouterLink> -->
+    </nav>
   </div>
 </template>
 
 <script setup>
+import { RouterLink } from 'vue-router';
+
+
 
 </script>
 
