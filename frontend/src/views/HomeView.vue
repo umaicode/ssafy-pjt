ed20f515ac99584734eab8fa38a58bea19bb655b
hyeonjim <sahacar2000@naver.com>
2025-12-21 02:12:24 +0900

    fix: 홈메뉴고정

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,15 +1,7 @@
 <template>
   <div>
     <!-- 메뉴 -->
-    <nav>
-      <RouterLink :to="{name:'ProductView'}">금융상품</RouterLink> | 
-      <RouterLink :to="{name: 'AnalysisView'}">분석하기</RouterLink> | 
-      <RouterLink :to="{name:'ProfileView'}">마이페이지</RouterLink> |
-      <RouterLink :to="{name:'KakaoMapView'}">은행찾기</RouterLink> |
-      <RouterLink :to="{name:'NewsView'}">뉴스</RouterLink> |
-      <RouterLink :to="{ name: 'YoutubeLayoutView' }">YouTube</RouterLink> |
-      <RouterLink :to="{ name: 'MetalView' }">현물상품</RouterLink> |
-    </nav>
+
   </div>
 </template>
 
